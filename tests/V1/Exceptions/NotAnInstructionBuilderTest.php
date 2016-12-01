db4f68fd4628bf52e0d8a7d1b9c7a05172ef45f2
Stuart Herbert <stuart@stuartherbert.com>
2016-12-01 11:46:41 +0000

    Fix: use correct namespace for InstructionBuilder interface, in exception message

--- tests/V1/Exceptions/NotAnInstructionBuilderTest.php
+++ tests/V1/Exceptions/NotAnInstructionBuilderTest.php
@@ -182,7 +182,7 @@ class NotAnInstructionBuilderTest extends PHPUnit_Framework_TestCase
 
         $data = "NotABuilder";
 
-        $expectedMessage = 'ReflectionMethod->invokeArgs(): GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromInputParameter()@199 says \'NotABuilder\' does not implement the \'GanbaroDigital\InstructionPipeline\V1\InstructionBuilder\' interface';
+        $expectedMessage = 'ReflectionMethod->invokeArgs(): GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromInputParameter()@199 says \'NotABuilder\' does not implement the \'GanbaroDigital\InstructionPipeline\V1\InstructionBuilders\InstructionBuilder\' interface';
         $expectedData = [
             'thrownBy' => new CodeCaller(self::class, __FUNCTION__, '->', __FILE__, __LINE__ + 12),
             'thrownByName' => 'GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromInputParameter()@199',
@@ -218,7 +218,7 @@ class NotAnInstructionBuilderTest extends PHPUnit_Framework_TestCase
 
         $data = 'NotABuilder';
 
-        $expectedMessage = 'GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromVariable()@233: \'NotABuilder\' does not implement the \'GanbaroDigital\InstructionPipeline\V1\InstructionBuilder\' interface';
+        $expectedMessage = 'GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromVariable()@233: \'NotABuilder\' does not implement the \'GanbaroDigital\InstructionPipeline\V1\InstructionBuilders\InstructionBuilder\' interface';
         $expectedData = [
             'thrownBy' => new CodeCaller(self::class, __FUNCTION__, '->', __FILE__, __LINE__ + 10),
             'thrownByName' => 'GanbaroDigitalTest\InstructionPipeline\V1\Exceptions\NotAnInstructionBuilderTest->testCanCreateFromVariable()@233',
